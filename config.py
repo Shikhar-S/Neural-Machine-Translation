836f7834eb6b5446e2ecd18c6d0f702bd950585d
Shikhar <shikhar.coder@gmail.com>
2020-09-05 15:05:33 +0530

    fix dic bug

--- config.py
+++ config.py
@@ -58,6 +58,6 @@ def get_args():
     args,unparsed = parser.parse_known_args()
     logger.info('__INIT__',extra=args.exec_id)
     global writer
-    writer = SummaryWriter('logs/'+args.exec_id)
+    writer = SummaryWriter('logs/'+args.exec_id['run'])
     log_parsed_args(args)
     return args, unparsed
